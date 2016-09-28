d54e7ec5f42102d396275329ed7ea83b09c88ce0
fauzi mohd <M.fauzi_87@hotmail.com>
2016-09-29 00:10:34 +0800

    timer completed

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,33 @@
 // your code here:
+var start = document.querySelector('#start')
+var reset = document.querySelector('#reset')
+var pause = document.querySelector('#pause')
+// var body = document.querySelector('body')
+
+ start.addEventListener('click', increMent);
+ reset.addEventListener('click', resetTime);
+ pause.addEventListener('click', pauseTime);
+
+var seconds = 0;
+var timerId;
+
+function updateTime(){
+
+  var head1 = document.querySelector('#timer')
+  head1.textContent = 'Timer: ' + seconds + 's'
+  seconds++
+}
+
+function increMent(){
+ timerId = window.setInterval(updateTime, 1000);
+}
+
+function pauseTime(){
+  window.clearInterval(timerId)
+}
+
+function resetTime(){
+  var head1 = document.querySelector('#timer')
+  head1.textContent = 'Stop Watch'
+  seconds = 0
+}
